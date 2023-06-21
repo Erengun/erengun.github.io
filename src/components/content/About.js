5c38da8168e1d2417d1e1b9e978527a2a5c6386b
Eren GUN <erengun4@gmail.com>
2023-06-21 11:27:21 +0300

    Update ux of About page

--- src/components/content/About.js
+++ src/components/content/About.js
@@ -13,14 +13,15 @@ const useStyles = makeStyles((theme) => ({
 		alignItems: 'center',
 		backgroundColor: theme.palette.background.default,
 	},
-	leftSection: {
-		flex: 1,
-	},
-	rightSection: {
-		flex: 1,
-	},
 	chip: {
 		margin: theme.spacing(0.5),
+	},
+	listItemSelected: {
+		backgroundColor: theme.palette.action.selected,
+		borderLeft: `4px solid ${theme.palette.primary.main}`,
+	},
+	card: {
+		height: '100%',
 	},
 }));
 
@@ -34,17 +35,19 @@ export const AboutSection = () => {
 
 	return (
 		<Container component="main" className={classes.container} maxWidth="md">
+			<Typography variant="h4" component="h1" gutterBottom>
+				About
+			</Typography>
 			<Paper component="section" elevation={3} square>
 				<Box p={4}>
-					<Typography variant="h2" component="h1" gutterBottom>
-						About
-					</Typography>
-					<Typography variant="body1" component="p" gutterBottom>
+					<Typography variant="body1" component="p" gutterBottom style={{ fontWeight: 300 }}>
 						{Resume.basics.description}
 					</Typography>
-					<Typography variant="body1" component="p" gutterBottom>
-						{Resume.basics.summary}
-					</Typography>
+					<Box mt={2}>
+						<Typography variant="body1" component="p" gutterBottom style={{ fontWeight: 300 }}>
+							{Resume.basics.summary}
+						</Typography>
+					</Box>
 				</Box>
 			</Paper>
 
@@ -56,7 +59,12 @@ export const AboutSection = () => {
 					<Grid item xs={12} sm={4}>
 						<List>
 							{Resume.work.map((experience, index) => (
-								<ListItem button key={index} onClick={() => handleExperienceClick(index)}>
+								<ListItem
+									button
+									key={index}
+									onClick={() => handleExperienceClick(index)}
+									className={expandedExperience === index ? classes.listItemSelected : ''}
+								>
 									<ListItemText primary={`${experience.company}`} />
 								</ListItem>
 							))}
@@ -65,22 +73,22 @@ export const AboutSection = () => {
 					<Grid item xs={12} sm={8}>
 						{Resume.work.map((experience, index) => (
 							<Box key={index} mb={2} style={{ display: expandedExperience === index ? 'block' : 'none' }}>
-								<Card>
+								<Card className={classes.card}>
 									<CardContent>
 										<Typography variant="h6" component="h3" gutterBottom>
-											{experience.position} at {experience.company}
+											{experience.position}
 										</Typography>
-										<Typography variant="body1" component="p" gutterBottom>
+										<Typography variant="body1" component="p" gutterBottom style={{ fontWeight: 300 }}>
 											{experience.startDate} - {experience.endDate || 'Present'}
 										</Typography>
-										<Typography variant="body1" component="p" gutterBottom>
+										<Typography variant="body1" component="p" gutterBottom style={{ fontWeight: 300 }}>
 											{experience.summary}
 										</Typography>
 										<TextField
 											fullWidth
 											multiline
+											rows={4}
 											variant="outlined"
-											label="Highlights"
 											value={experience.highlights}
 											InputProps={{ readOnly: true }}
 										/>
